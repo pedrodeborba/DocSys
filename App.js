c32305f7b63c89f2797b9fb52439466a506bc296
Pedro de Borba <pedrodeborba1709@gmail.com>
2023-06-25 12:01:15 -0300

    Definindo as rotas

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import FirstScreen from './src/pages/FirstScreen'
+import Paciente from './src/pages/Paciente';
+import Profissional from './src/pages/Profissional';
+import Home from './src/pages/Home';
 
-export default function App() {
+const Stack = createStackNavigator();
+function MyStack() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator initialRouteName="FirstScreen">
+      <Stack.Screen name="FirstScreen" component={FirstScreen} />
+      <Stack.Screen name="Paciente" component={Paciente} />
+      <Stack.Screen name="Profissional" component={Profissional} />
+      <Stack.Screen name="Home" component={Home} /> 
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
